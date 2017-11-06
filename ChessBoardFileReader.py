1d767f594f3c881933733eee8fdf8bc9c944a5f4
denise <denise.goldan@gmail.com>
2017-11-06 20:29:42 +0200

    Created matrix builder (from file) class

---
+++ ChessBoardFileReader.py
@@ -0,0 +1,19 @@
+class ChessBoardFileReader:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def extract_chess_board_from_file(filename):
+        descriptor = open(filename, "r")
+        return [map(int,line.split(",")) for line in descriptor]
+
+    @staticmethod
+    def print_chess_board(chess_board):
+        for line in chess_board:
+            print line
+
+
+if __name__ == '__main__':
+    chess_board = ChessBoardFileReader.extract_chess_board_from_file("input-matrix.txt")
+    ChessBoardFileReader.print_chess_board(chess_board)
