b13e1e1a46d8e4f02c34d3144145bafcc4561a2b
Corneliu Soficu <corneliu.soficu@gmail.com>
2017-11-07 18:29:08 +0200

    Created human readable chessboard print.

--- chessboard.py
+++ chessboard.py
@@ -2,8 +2,21 @@
 """ Let the white pawn be represented by 1 and the black pawn by 2.
 We agree on the fact that the top of the chess board is the start point for the white pawns
 and the bottom of the chess board is the start for the black ones. """
+from __future__ import print_function
 
+import sys
 from chess_board_file_reader import ChessBoardFileReader
+from termcolor import colored, cprint
+
+
+CHESS_BOARD_SIZE = 8
+
+BLACK_PAWN_UNICODE = u'\u265F'
+WHITE_PAWN_UNICODE = u'\u2659'
+GREY_CHESSBOARD_CELL  = '\x1b[0;30;47m' + '%s' + '\x1b[0m'
+WHITE_CHESSBOARD_CELL = '%s'
+CUSTOM_WIDTH_SPACE = u'\u2003'
+CELL_COLORS = [WHITE_CHESSBOARD_CELL, GREY_CHESSBOARD_CELL]
 
 class ChessBoard:
 
@@ -42,7 +55,25 @@ class ChessBoard:
         return black_pawns_line, empty_line, white_pawns_line
 
     def print_chessboard(self):
-        print 'Test'
+        color_index = 0
+        color_inx = 0
+        colors_size = len(CELL_COLORS)
+        for chessboard_line in self.chess_board:
+            color_index = color_inx
+            for item in chessboard_line:
+                chessboard_item = None
+                if item == 1:
+                    chessboard_item = WHITE_PAWN_UNICODE
+                elif item == 2:
+                    chessboard_item = BLACK_PAWN_UNICODE
+                else:
+                    chessboard_item = CUSTOM_WIDTH_SPACE
+                chessboard_item = CELL_COLORS[color_index % colors_size] % format(chessboard_item)
+                print (chessboard_item, end='')
+                color_index += 1
+            color_inx += 1
+            print()
+
 
 if __name__ == '__main__':
     example_board = ChessBoard()
